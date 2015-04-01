ac7b9fed508258af9e31850cfc03a5cd47089877
unknown <j.anaya.villagrana@0262-VILLAGRANA.CALHEERS.LOCAL>
2015-04-01 12:32:44 -0700

    reposritory fixed, added game created, git ignore file, space ship, and actions to control the space ship, also added the corrected version of the player controller, this fixed the bounderies issue.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour {
+	private static int currentLevel = 1;
+	public Text levelTextValue;
+	public Text levelTextCounter;
+	private int seconds;
+
+	private class TimerThread{
+		private LevelManager levelManager;
+
+		public TimerThread(LevelManager levelManager){
+			this.levelManager = levelManager;
+		}
+
+		public void DoWork(){
+			while (levelManager.seconds > 0) {
+				System.Threading.Thread.Sleep(1000);
+				levelManager.seconds--;
+			}
+		}
+	}
+
+	void Start(){
+	}
+
+	public void resetLevel(){
+		currentLevel = 1;
+	}
+
+	public void LoadLevel(string name){
+		Application.LoadLevel (name);
+	}
+
+	public void Update(){
+		//IF LEVEL LOADED IS LEVEL INTRODUCTION THEN CHECK IF THE THREAD FINISHED DEVREMENTING THE COUNTER SO IT AUTOMATICALLY
+		//LOADS LEVEL 01
+		if (Application.loadedLevel == 1) {
+			levelTextCounter.text = seconds.ToString();
+			if(seconds == 0){
+				//LOADS NEXT LEVEL WHEN THE SECONDS IS EQUIAL TO CERO
+				//TODO: LOADS FOR NOW THE WIN SCREEN
+				LoadLevel("Win Screen");
+			}
+		}
+	}
+
+	public void OnLevelWasLoaded(int level){
+		/*
+		 * IF LEVEL LOADED IS EQUAL TO LEVEL INTRODUCTION THEN EXECUTE THE WAIT THREAD
+		 */
+		if (level == 1) {
+			levelTextValue.text = currentLevel.ToString ();
+			//WAIT 5 SECONDS TO LOAD THE NEXT LEVEL
+			seconds = 5;
+			TimerThread tt = new TimerThread(this);
+			System.Threading.Thread timerThread = new System.Threading.Thread(tt.DoWork);
+			//STARTS THREAD
+			timerThread.Start();
+		}
+	}
+	
+	public void QuitRequest(){
+		//WILL NOT WORK FOR WEB APPLICATIONS
+		Application.Quit ();
+	}
+	
+	public void loadNextLevel(int levelIndex){
+		//LOADS A LEVEL BASED ON AN INDEX
+		Application.LoadLevel (levelIndex);
+	}
+
+
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer musicPlayer;
+
+	void Awake(){
+		Debug.Log ("Awake: " + GetInstanceID ());
+		if (musicPlayer != null) {
+			Destroy (gameObject);
+		} else {
+			musicPlayer = this;
+		}
+		GameObject.DontDestroyOnLoad (gameObject);
+	}
+}
