7643b65e2e1d887690169f5d6a21c5c9b40bc505
unknown <j.anaya.villagrana@0262-VILLAGRANA.CALHEERS.LOCAL>
2015-04-01 18:22:30 -0700

    created a new enemy prefab, with its script. Created script EnemySpawner, this script will be in charge to spawn the enemies in the game.

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+	// Use this for initialization
+	void Start () {
+		//this line creates a new isntance of the game object enemyPrefab, which is initialized from the UI
+		GameObject enemy = Instantiate (enemyPrefab, new Vector3 (8, 6, 0), Quaternion.identity) as GameObject;
+		//this line makes the new instance created, be part of the EnemySpawner game object, 
+		/*
+		 * IN UI
+		 * EnemySpawner
+		 * 		|	
+		 * 		|-->Enemy(clone) : this is the game object created from the prefab
+		 */
+		enemy.transform.parent = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
