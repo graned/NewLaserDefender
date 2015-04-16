b0f3a4800d260adc849413beb68081f802124a38
unknown <j.anaya.villagrana@0262-VILLAGRANA.CALHEERS.LOCAL>
2015-04-15 17:49:28 -0700

    laser issue corrected.

--- Assets/Scripts/LaserController.cs
+++ Assets/Scripts/LaserController.cs
@@ -1,34 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LaserController{
-	private Vector3 laserVector;
-	private GameObject laserObject;
-	private float laserSpeed;
+public class LaserController : MonoBehaviour{
 	private MovementController movController;
-	private bool laserFired = false;
-
-	public LaserController(GameObject laserObject, float laserSpeed){
-		this.laserObject = laserObject;
-		this.laserSpeed = laserSpeed;
-	}
-	// Use this for initialization
-	void Start () {
-		movController = new MovementController (laserObject);
+	public float laserSpeed = 15f;
+	
+	public void Start(){
+		movController = new MovementController (this.gameObject);
 		movController.MovementSpeed = this.laserSpeed;
-		movController.ObjectToMoveCurrentPosition = laserObject.transform.position;
+		movController.ObjectToMoveCurrentPosition = this.transform.position;
 		movController.defineWorldBounds (Camera.main);
+	}	
+
+	void Update(){
+		fireLaser ();
 	}
-	public void Update(){
-		if (laserFired) {
-			movController.moveObject (0f, laserSpeed);
-		}
+	
+	// FIRES THE LASER!
+	public void fireLaser(){
+		Debug.Log("FIRE THE \"LASER\"");
+		movController.moveObject (0f, laserSpeed);
 	}
 
-	// FIRES THE LASER!
-	public void fireLaser(GameObject laserToMove){
-		Debug.Log("FIRE THE \"LASER\"");
-		movController.ObjectToMove = laserToMove;
-		laserFired = true;
-	}
+
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,20 +3,17 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	public float movementSpeed; //THIS VARIABLE WILL BE CONTROLLED FROM THE EDITOR
-	public GameObject laserPrefab;
 	public GameObject spaceShip;
-	public float laserSpeed = 15f;
 	private MovementController movController;
-	private LaserController laserController;
-
+	public GameObject laserPrefab;
 
 	// Use this for initialization
 	void Start () {
 		movController = new MovementController (spaceShip);
 		movController.ObjectToMoveCurrentPosition = spaceShip.transform.position;
 		movController.MovementSpeed = movementSpeed;
+		movController.ObjectToMoveCurrentPosition = this.transform.position;
 		movController.defineWorldBounds (Camera.main);
-		laserController = new LaserController (laserPrefab, laserSpeed);
 	}
 			
 	// Update is called once per frame
@@ -24,24 +21,26 @@ public class PlayerController : MonoBehaviour {
 		movController.arrowControl ();
 		KeyPressed ();
 	}	
-
 	private void KeyPressed(){
 		if(Input.GetKeyDown(KeyCode.Space)){
 			//SPAWN LASER BEAM & FIRE LASER!
-			spawnFireLaser(transform);
+			Debug.Log("FIRE THE \"LASER\"");
+			spawnLaser(transform);
 		}
 	}
 
 	/*
 	 * THIS METHOD SPAWNS LASER BEAM
 	 */
-	private void spawnFireLaser(Transform trans){
-		foreach (Transform t in trans) {
+	private void spawnLaser(Transform transParent){
+		foreach(Transform t in transParent){
 			//this line creates a new isntance of the game object enemyPrefab, which is initialized from the UI
-			GameObject laser = Instantiate (laserPrefab, t.position, Quaternion.identity) as GameObject;
+			Vector3 position = t.position;
+			position.z -= 5;
+			GameObject laser = Instantiate (laserPrefab, position, Quaternion.identity) as GameObject;
 			//assignes the new instance created a parent
 			laser.transform.parent = t;
-			laserController.fireLaser (laser);
 		}
 	}
+	
 }
